e8777079212670c7161939337c0252ea9e59993d
Rylan Jurgens <122000590+gatekeeperR2000@users.noreply.github.com>
2024-01-26 17:51:32 -0700

    added a .h file for the ToDoList class

---
+++ ToDoList.h
@@ -0,0 +1,24 @@
+// Compile Once
+#pragma once
+
+// Maybe redundant make sure only one definition of this file
+#ifndef ToDoList_h
+#define ToDoList_h
+
+// Includes
+#include <string>
+#include <iostream>
+#include <list>
+
+class ToDoList {
+private:
+	// Private Variables
+
+public:
+	// Public functions
+
+
+};
+
+#endif
+
